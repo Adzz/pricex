61e81ac30e3f2b25dcf8c460af36eb225550e589
Adam Lancaster <lancaster28@hotmail.com>
2018-07-17 22:44:10 +0100

    scrapes prices from AO successfully

--- lib/a_o.ex
+++ lib/a_o.ex
@@ -1,19 +1,38 @@
 defmodule AO do
+  @index_page_images_id "#hypImage"
+  @price_selector "span[itemProp=\"price\"]"
+
   def scrape(uri) do
     uri
     |> URI.to_string()
-    |> HTTPoison.get()
-    |> case do
-      {:ok, res = %{status_code: 200}} -> parse_body(res.body)
-      error -> IO.inspect(error, label: "Error!")
-    end
+    |> HTTPoison.get!()
+    |> parse_body()
+    |> crawl_page()
   end
 
-  defp parse_body(body) do
-    body
+  defp parse_body(response = %{status_code: 200}) do
+    response.body
     |> Floki.parse()
-    |> Floki.find("#hypImage")
+  end
+
+  defp parse_body(error) do
+    IO.inspect(error, label: "Error!, response code not 200!")
+  end
+
+  defp crawl_page(body) do
+    body
+    |> Floki.find(@index_page_images_id)
     |> Floki.attribute("href")
+    |> Enum.map(fn path ->
+      HTTPoison.get!("https://ao.com#{path}") |> parse_body |> extract_information
+    end)
+  end
+
+  defp extract_information(body) do
+    body
+    |> Floki.find(".price")
+    |> Floki.find("span")
+    |> Floki.attribute("content")
     |> IO.inspect()
   end
 end
